b03d109e068f94b622275d03f3912217e927292a
Divyansh Agarwal <divyanshagarwal97@gmail.com>
2017-09-24 18:54:16 -0700

    Update comprehensive_model.py

--- comprehensive_model.py
+++ comprehensive_model.py
@@ -32,7 +32,7 @@ class ComprehensiveModel(object):
         self.padded_y_windows = padded_y_windows
         self.test_padded_y_windows = test_padded_y_windows
 
-    # STEP 3: Construct non-stateful LSTM
+    # Construct non-stateful LSTM
     def construct_model(self, e_size, hidden_size, layers, lrate, opt):
         X = self.X
         y = self.y
@@ -105,7 +105,7 @@ class ComprehensiveModel(object):
             lrate) + '_' + str(e_size) + '_' + o + '_'
         return model, modelname
 
-    # STEP 3: Construct stateful LSTM
+    # Construct stateful LSTM
     def construct_stateful_model(self, e_size, hidden_size, layers, lrate, opt, max_len, stateful=True, sw = False):
         X = self.X
         y = self.y
@@ -148,73 +148,9 @@ class ComprehensiveModel(object):
         print("SUccess")
         return model
 
-    '''def construct_stateful_sw(self, e_size, hidden_size, layers, lrate, opt, max_len, stateful=True,
-                                     window_shifting=False):
-        X = self.X
-        y = self.y
-        vocab_size = self.vocab_size
-        max_len = self.max_len
-        padding_size = self.padding_size
-        padded_y_windows = self.padded_y_windows
-
-        e_size = e_size
-        HIDDEN_SIZE = hidden_size
-        LAYERS = layers
-        lrate = lrate
-        print('building a model')
-        model = Sequential()
-
-        if (stateful):
-            num_timesteps = int(padding_size / 10)
-            model.add(Embedding(vocab_size + 1, e_size, batch_input_shape=(64, num_timesteps), mask_zero=True))
-
-        else:
-            if window_shifting:
-                window_len = max_len / 10
-            model.add(Embedding(vocab_size + 1, e_size, mask_zero=True))
-
-        for i in range(LAYERS):
-            print("adding layer " + str(i))
-            # print(len(trainuserids))
-            model.add(LSTM(HIDDEN_SIZE, return_sequences=True, dropout_W=0.2, stateful=True))
-
-        # if i == (LAYERS - 1):
-        #            model.add(Dropout(.2))
-        model.add(TimeDistributed(Dense(vocab_size)))
-        model.add(Activation('softmax'))
-        opt = opt(lr=lrate)
-        model.compile(loss='categorical_crossentropy', optimizer=opt, sample_weight_mode='temporal', metrics=['categorical_crossentropy'])
-        if isinstance(opt, Adagrad):
-            o = 'adagrad'
-        elif isinstance(opt, RMSprop):
-            o = 'rmsprop'
-
-        return model'''
-
-
 
     # STEP 4: Train model
-    # May want to output accuracies and other metrics during training
-    """
-    class WriteResults(Callback):
-        def __init__(self, monitor='val_acc',file_to_write, model_params):
-            self.file_name = file_to_write
-            self.monitor = monitor
-            self.params = model_params
-
-        def on_epoch_end(self, epoch, logs={}):
-            current = logs.get(self.monitor)
-            if current is None:
-                warnings.warn("val acc unavailable")
-
-            with open(file_to_write, 'a') as csvfile:
-                resultswriter = csv.writer(csvfile)
-                towrite = [str(val_acc)] + self.params
-                resultswriter.writerow(towrite)
-    """
-    '''def accuracy_metric(y_true, y_pred):
-        if(y_true == 0): '''
-
+   
     def compute_val_acc(self, model):
         test_x = self.testX
         test_y = self.test_padded_y_windows
@@ -281,23 +217,10 @@ class ComprehensiveModel(object):
         print("Per student accuracy: ", per_student_accuracies)
         # print("Total validation accuracy:", total_val_acc)
         print("Average of student accuracies:", np.mean(per_student_accuracies_copy))
-        '''total_val_acc = float(total_correct) / (total_correct + total_incorrect)
-            #print("Total validation accuracy:", total_val_acc)
-            print("Student Correct ", current_student_correct)
-            print("Student Incorrect ", current_student_incorrect)
-            print("Average of student accuracies:", np.mean(per_student_accuracies))
-            return total_val_acc, per_student_accuracies'''
+        
         return float(total_correct) / (total_correct + total_incorrect), per_student_accuracies_copy
 
-    '''def sample_weights_validation(self):
-        #validation_i = self.validation_i
-
-        sample_weights = np.zeros(len(self.X) - validation_i)
-        validation_set = self.X[validation_i:]
-        for i, x in enumerate(validation_set):
-            sample_weights[i] = 1 / np.count_nonzero(x)
-        return sample_weights'''
-
+   
     def compute_val_acc_sw(self, model):
         test_x = self.testX
         test_y = self.test_padded_y_windows
@@ -343,8 +266,7 @@ class ComprehensiveModel(object):
 
             per_student_accuracies.append(acc)
 
-        #sample_weights_val = self.sample_weights_validation()
-        #weighted_accs = per_student_accuracies * sample_weights_val
+        
         total_val_acc = float(total_correct) / (total_correct + total_incorrect)
         # print("Total validation accuracy:", total_val_acc)
         print("Average of student accuracies:", np.mean(per_student_accuracies))
@@ -442,9 +364,7 @@ class ComprehensiveModel(object):
                 np.random.shuffle(batches_y)
                 # print(X[:2])
 
-            # model.fit(X[:validation_i], y[:validation_i], batch_size=64, nb_epoch=10,
-            #          validation_data=(X[validation_i:], y[validation_i:]))
-
+            
             for i in zip(batches_x, batches_y):
                 #num_data_points += np.count_nonzero(x)
 
@@ -466,9 +386,8 @@ class ComprehensiveModel(object):
         # print("Accuracy: ", acc)
         return acc, per_student_acc
 
-        # train sorted non-stateful LSTM with custom sample weights
-
-    '''def sample_weights(self):
+    # train sorted non-stateful LSTM with custom sample weights
+    def sample_weights(self):
         lengths = np.apply_along_axis(np.count_nonzero, 1, self.X)
         min_length = np.argmin(lengths)
 
@@ -480,7 +399,7 @@ class ComprehensiveModel(object):
 
         print("SW shape", np.apply_along_axis(helper, 1, self.X).shape)
 
-        return np.reshape(np.apply_along_axis(helper, 1, self.X).shape[0], 1)'''
+        return np.reshape(np.apply_along_axis(helper, 1, self.X).shape[0], 1)
     def train_model(self, model, modelname, batchsize, epochs, shuffle = True, sw = False):
         X = self.X
         y = self.y
@@ -515,11 +434,7 @@ class ComprehensiveModel(object):
         # print("Accuracy: ", acc)
         return acc, per_student_acc
 
-
-
-
-
-    '''def train_with_sw(self, model, modelname, batchsize, epochs):
+    def train_with_sw(self, model, modelname, batchsize, epochs):
         X = self.X
         y = self.y
         validation_i = self.validation_i
@@ -534,91 +449,7 @@ class ComprehensiveModel(object):
         model.fit(X[:validation_i], y[:validation_i], batch_size=64, nb_epoch=10,
                   validation_data=(X[validation_i:], y[validation_i:]), sample_weight=sampleWeight)
         acc, per_student_acc = self.compute_val_acc_sw(model)
-        return acc, per_student_acc'''
-
-    '''def compute_val_acc_stateful(self, model, batchsize, num_timesteps, max_len, validation=True):
-        X = self.testX
-        y = self.testY
-
-        #test_x = self.testX
-        #test_y = self.testY
-        validation_i = self.validation_i
-        vocab_size = self.vocab_size
-        max_len = self.max_len
-        padding_size = self.padding_size
-        padded_y_windows = self.padded_y_windows
-
-
-
-        print("Computing Validation Hill Climbing Accuracy")
-        total_correct = 0
-        total_incorrect = 0
-        if (validation_i):
-            per_student_accuracies = np.zeros(len(X) - validation_i)
-            student_correct = np.zeros(len(X) - validation_i)
-            student_incorrect = np.zeros(len(X) - validation_i)
-            for j in range(validation_i, len(X), batchsize):  # go over all batches
-                # current_student_correct = 0
-                # current_student_incorrect = 0
-                for k in range(0, max_len, num_timesteps):  # go over all the sequences
-
-                    # if(k + num_timesteps <= max_len):
-                    test_x = X[j:j + batchsize, k:k + num_timesteps]
-                    # else:
-                    test_y = padded_y_windows[j: j + batchsize, k: k + num_timesteps]
-
-                    if (j + batchsize > validation_i):
-                        # print("At this val j greater", j)
-                        x = X[j:validation_i, k:k + num_timesteps]
-                        small_y = y[j:validation_i, k:k + num_timesteps]
-                    if (len(test_x) < 128):
-                        # curr =
-                        diff = 128 - len(test_x)
-                        temp = np.zeros((diff, num_timesteps))
-                        test_x = np.concatenate((test_x, temp))
-                        test_y = np.concatenate((test_y, temp))
-
-                    predictions = model.predict_on_batch(test_x)  # predicts for batch size of 64
-                    for i, x in enumerate(test_x):
-                        current_predictions = list(predictions[i])
-                        # print("Current predictions", current_predictions[:10])
-                        current_answers = list(test_y[i])
-
-                        for l in range(len(x)):
-                            current_value = x[l]
-                            if current_value == 0:
-                                continue
-                            else:
-                                current_softmax = list(current_predictions[l])
-                                # print("Current softmax", current_softmax[:10])
-                                # print("index: ", current_softmax.index(max(current_softmax)))
-                                best_prediction = current_softmax.index(max(current_softmax))
-                                correct_answer = current_answers[l]
-                                if best_prediction == correct_answer:
-                                    student_correct[i] += 1
-                                    total_correct += 1
-                                else:
-                                    student_incorrect[i] += 1
-                                    total_incorrect += 1
-                    model.reset_states()
-
-        # acc = round(current_student_correct) / (current_student_incorrect + current_student_correct)
-        # per_student_accuracies.append(acc)
-
-        totals = student_correct + student_incorrect
-        per_student_accuracies = student_correct / totals
-        print("Total: ", totals)
-        print("Student Correct: ", student_correct)
-        print("Studen Incorrect:", student_incorrect)
-
-        total_val_acc = total_correct / totals
-        per_student_accuracies_copy = per_student_accuracies[
-            np.isfinite(per_student_accuracies)]  # filters out all nans
-        print("Per student accuracy: ", per_student_accuracies_copy)
-        # print("Total validation accuracy:", total_val_acc)
-        print("Average of student accuracies:", np.mean(per_student_accuracies_copy))
-
-        return float(total_correct) / (total_correct + total_incorrect), per_student_accuracies'''
+        return acc, per_student_acc
 
     def final_accuracy_stateful(self, model, batchsize, num_timesteps, max_len):
         X = self.testX
@@ -688,21 +519,7 @@ class ComprehensiveModel(object):
         print("Average of student accuracies:", np.mean(per_student_accuracies_copy))
         return float(total_correct) / (total_correct + total_incorrect), per_student_accuracies_copy
 
-    '''def sort(self, userids):
-        userid_len_mapping = {}  # len(elem): elem for elem in list(userids)}
-        for elem in list(userids):
-            try:
-                userid_len_mapping[len(elem)].append(elem)
-            except KeyError:
-                userid_len_mapping[len(elem)] = [elem]
-        # lengths = list(userid_len_mapping.keys())
-        # sorted_dict = OrderedDict(sorted(userid_len_mapping.items(), key=lambda t: t[0]))
-        result = []
-        for key in userid_len_mapping.keys():
-            for elem in userid_len_mapping[key]:
-                result.append(elem)
-        return np.array(result)'''
-
+    
     def train_stateful_sorted(self, model, modelname, batchsize, epochs, shuffle = True, sw = False):
         '''Assume self.X is sorted'''
         X = np.copy(self.X)
